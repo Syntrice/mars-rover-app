8f290a4e21eb0acc31102516e7833153dd2f0692
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-12-05 11:17:01 +0000

    feat(input): add static class InputParser
    Add InputParser which has static methods used to parse string inputs
    to their corresponding data types.

---
+++ MarsRoverApp/RoverInput/InputParser.cs
@@ -0,0 +1,20 @@
+﻿namespace MarsRoverApp.RoverInput
+{
+    public static class InputParser
+    {
+        public static PlateauSize ParsePlateauSizeString(string input)
+        {
+            return new PlateauSize(0, 0);
+        }
+
+        public static Position ParsePositionString(string input)
+        {
+            return new Position(0, 0, Direction.North);
+        }
+
+        public static RoverInstruction ParseRoverInstructionString(string input)
+        {
+            return RoverInstruction.MoveForward;
+        }
+    }
+}
