bf4a361da531d3704a315742615047aabb4b4f9d
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-12-05 11:01:04 +0000

    feat(input): create position struct to store coordinates and direction

---
+++ MarsRoverApp/RoverInput/Position.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverApp.RoverInput
+{
+    public struct Position
+    {
+        public int x { get; }
+        public int y { get; }
+        public Direction direction { get; }
+
+        public Position(int x, int y, Direction direction)
+        {
+            this.x = x;
+            this.y = y;
+            this.direction = direction;
+        }
+    }
+}
