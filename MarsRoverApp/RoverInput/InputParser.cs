f418e4148cdc004ee895fb09ecb48fc41f5d7a6d
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-12-05 12:16:17 +0000

    feat(input): satisfy happy-paths for InputParser
    Implement InputParser methods to satisfy all happy-path tests.

--- MarsRoverApp/RoverInput/InputParser.cs
+++ MarsRoverApp/RoverInput/InputParser.cs
@@ -4,17 +4,41 @@
     {
         public static PlateauSize ParsePlateauSize(string input)
         {
-            return new PlateauSize(0, 0);
+            string[] coords = input.Split(' ');
+            int width = int.Parse(coords[0]) + 1;
+            int height = int.Parse(coords[1]) + 1;
+
+            return new PlateauSize(width, height);
         }
 
         public static RoverPosition ParseRoverPosition(string input)
         {
-            return new RoverPosition(0, 0, Direction.North);
+            string[] position = input.Split(" ");
+            int x = int.Parse(position[0]);
+            int y = int.Parse(position[1]);
+
+            Direction direction = position[2] switch
+            {
+                "N" => Direction.North,
+                "E" => Direction.East,
+                "S" => Direction.South,
+                "W" => Direction.West
+            };
+
+            return new RoverPosition(x, y, direction);
         }
 
         public static RoverInstruction[] ParseRoverInstructions(string input)
         {
-            return new RoverInstruction[0];
+            var roverInstructions = input.Select(c => c switch
+            {
+                'L' => RoverInstruction.TurnLeft,
+                'R' => RoverInstruction.TurnRight,
+                'M' => RoverInstruction.MoveForward
+            }
+            );
+
+            return roverInstructions.ToArray();
         }
     }
 }
