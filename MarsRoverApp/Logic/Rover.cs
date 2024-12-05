98228a6b4948d50f9de706202aaa44586e9559d1
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-12-05 15:21:12 +0000

    feat(logic): Add rotation functionality to Rover
    Correctly process TurnLeft and TurnRight instructions so that the
    Rover rotates correctly and all tests pass

--- MarsRoverApp/Logic/Rover.cs
+++ MarsRoverApp/Logic/Rover.cs
@@ -13,7 +13,16 @@ namespace MarsRoverApp.Logic
 
         public void Instruct(RoverInstruction instruction)
         {
+            switch (instruction)
+            {
+                case RoverInstruction.TurnLeft:
+                    Direction = Direction == Direction.North ? Direction.West : Direction - 1;
+                    break;
+                case RoverInstruction.TurnRight:
+                    Direction = Direction == Direction.West ? Direction.North : Direction + 1;
+                    break;
 
+            }
         }
     }
 }
