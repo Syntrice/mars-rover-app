913288e6d31b75ac644bd4455d3df02a6d1b9fd2
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-12-05 15:35:58 +0000

    feat(logic): Add boilerplate Plateau class
    The Plateau class is a class that represents the plateau on which the
    rovers will be moving. It will be responsible for acknowledging the
    landing of rovers, and keeping track of their positions when they
    move.

---
+++ MarsRoverApp/Logic/Plateau.cs
@@ -0,0 +1,25 @@
+﻿using MarsRoverApp.Input;
+
+namespace MarsRoverApp.Logic
+{
+    public class Plateau
+    {
+        public PlateauSize Size { get; }
+
+        public Plateau(PlateauSize size)
+        {
+            Size = size;
+        }
+
+        public void LandRover(RoverPosition roverPosition)
+        {
+            // TODO
+        }
+
+        public Rover? GetRoverAtPos(int x, int y)
+        {
+            // TODO
+            return null;
+        }
+    }
+}
