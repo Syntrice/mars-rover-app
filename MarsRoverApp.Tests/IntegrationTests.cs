6c099c5cac40db748bcf67a2f82e9576492123c5
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-12-06 10:09:22 +0000

    test: update IntegrationTest to test for final output

--- MarsRoverApp.Tests/IntegrationTests.cs
+++ MarsRoverApp.Tests/IntegrationTests.cs
@@ -30,6 +30,9 @@ namespace MarsRoverApp.Tests
             RoverPosition rover1FinalPosition = new(1, 3, Direction.North);
             RoverPosition rover2FinalPosition = new(5, 1, Direction.East);
 
+            string expectedRover1Output = "1 3 N";
+            string expectedRover2Output = "5 1 E";
+
             // -- ACT --
 
             PlateauSize plateauSize = InputParser.ParsePlateauSize(plateauSizeString);
@@ -47,11 +50,18 @@ namespace MarsRoverApp.Tests
             RoverInstruction[] rover2Instructions = InputParser.ParseRoverInstructions(rover2Input.instructions);
             rover2.Instruct(rover2Instructions);
 
+            string actualRover1Output = OutputPaser.ParseRoverPosition(plateau.GetRoverPosition(rover1).Value);
+            string actualRover2Output = OutputPaser.ParseRoverPosition(plateau.GetRoverPosition(rover2).Value);
+
             // -- ASSERT --
             plateau.GetRoverAtPos(rover1FinalPosition.X, rover1FinalPosition.Y).Should().Be(rover1);
             plateau.GetRoverAtPos(rover2FinalPosition.X, rover2FinalPosition.Y).Should().Be(rover2);
             rover1.Direction.Should().Be(rover1FinalPosition.Direction);
             rover2.Direction.Should().Be(rover2FinalPosition.Direction);
+            actualRover1Output.Should().Be(expectedRover1Output);
+            actualRover2Output.Should().Be(expectedRover2Output);
+
+
 
         }
 
