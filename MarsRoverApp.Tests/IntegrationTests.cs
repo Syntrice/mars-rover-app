775db897631d9e95cf56ef8350dd64a65fbb7c50
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-12-06 09:41:18 +0000

    test: Add logic and input integration test
    Add an integration test to test the input and logic layers with valid input

---
+++ MarsRoverApp.Tests/IntegrationTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using MarsRoverApp.Input;
+using MarsRoverApp.Logic;
+
+namespace MarsRoverApp.Tests
+{
+    [TestFixture]
+    public class IntegrationTests
+    {
+        [Test]
+        public void InputWithLogic_ValidInput_CorrectRoverPositions()
+        {
+            // -- ARRANGE --
+            string[] input = [
+            "5 5",
+            "1 2 N",
+            "LMLMLMLMM",
+            "3 3 E",
+            "MMRMMRMRRM"
+            ];
+
+            // The first line of input will always be plateau size
+            string plateauSizeString = input[0];
+
+            // The following lines should be pairs of values, corresponding to a rovers
+            // initial position and instructions
+            (string initialPosition, string instructions) rover1Input = (input[1], input[2]);
+            (string initialPosition, string instructions) rover2Input = (input[3], input[4]);
+
+            RoverPosition rover1FinalPosition = new(1, 3, Direction.North);
+            RoverPosition rover2FinalPosition = new(5, 1, Direction.East);
+
+            // -- ACT --
+
+            PlateauSize plateauSize = InputParser.ParsePlateauSize(plateauSizeString);
+            Plateau plateau = new Plateau(plateauSize);
+
+            // Rover1
+            RoverPosition rover1Position = InputParser.ParseRoverPosition(rover1Input.initialPosition);
+            Rover rover1 = plateau.LandRover(rover1Position);
+            RoverInstruction[] rover1Instructions = InputParser.ParseRoverInstructions(rover1Input.instructions);
+            rover1.Instruct(rover1Instructions);
+
+            // Rover2
+            RoverPosition rover2Position = InputParser.ParseRoverPosition(rover2Input.initialPosition);
+            Rover rover2 = plateau.LandRover(rover2Position);
+            RoverInstruction[] rover2Instructions = InputParser.ParseRoverInstructions(rover2Input.instructions);
+            rover2.Instruct(rover2Instructions);
+
+            // -- ASSERT --
+            plateau.GetRoverAtPos(rover1FinalPosition.X, rover1FinalPosition.Y).Should().Be(rover1);
+            plateau.GetRoverAtPos(rover2FinalPosition.X, rover2FinalPosition.Y).Should().Be(rover2);
+            rover1.Direction.Should().Be(rover1FinalPosition.Direction);
+            rover2.Direction.Should().Be(rover2FinalPosition.Direction);
+
+        }
+
+    }
+}
